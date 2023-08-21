545f512a33d28fb7d02d6d5b2896459b95727283
MathiasKristiansen <mathias.kristiansen@nrc.no>
2023-08-21 12:09:25 +0200

    Added frontend for URL A/B splitter

--- script.js
+++ script.js
@@ -7,21 +7,44 @@ const port = 3000;
 // Middleware to parse JSON body
 app.use(bodyParser.json());
 
-// Route to split URL
-app.post('/split-url', (req, res) => {
-  const url = req.body.url;
-  if (!url) {
-    return res.status(400).json({ error: 'URL is required' });
-  }
+// In-memory storage for URLs and their unique IDs
+let urlStore = {};
 
-  // Create the two versions
-  const versionA = url + '/test-a';
-  const versionB = url + '/test-b';
+// Generate a random 5-digit ID
+function generateID() {
+    return Math.floor(10000 + Math.random() * 90000).toString();
+}
 
-  // Send the response
-  res.json({ versionA, versionB });
+// Route to create unique URL
+app.post('/generate-url', (req, res) => {
+    const originalUrl = req.body.url;
+    if (!originalUrl) {
+        return res.status(400).json({ error: 'URL is required' });
+    }
+
+    const id = generateID();
+    urlStore[id] = originalUrl;
+
+    res.json({ uniqueUrl: `http://localhost:${port}/${id}` });
+});
+
+// Route to handle user accessing the unique URL
+app.get('/:id', (req, res) => {
+    const id = req.params.id;
+    const originalUrl = urlStore[id];
+
+    if (!originalUrl) {
+        return res.status(404).send('URL not found');
+    }
+
+    const randomNumber = Math.random();
+    if (randomNumber < 0.5) {
+        res.redirect(originalUrl + `/${id}/A`);
+    } else {
+        res.redirect(originalUrl + `/${id}/B`);
+    }
 });
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
+    console.log(`Server running at http://localhost:${port}/`);
 });
